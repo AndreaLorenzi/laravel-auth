4afc849db2157a5d1af3ee6d9f5a180de307331a
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-07-06 15:10:14 +0200

    add nav

---
+++ resources/views/Admin/includes/nav.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar mb-5 navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <div>
+            <a class="navbar-brand" href="#">Navbar di Boolcoso</a>
+        </div>
+
+        <div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Index</a></li>
+                            <li><a class="dropdown-item" href="#">Add</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <form class="d-flex" action="">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</nav>
