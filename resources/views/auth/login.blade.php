1662687d3c54f3761dba5e234e2fcb33fb522db6
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-07-06 11:50:14 +0200

    fix register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
             <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" required
-                autofocus autocomplete="username">
+                autofocus autocomplete="username" value="{{ old('email') }}">
         </div>
         <div class="mb-3">
             <label for="password" class="form-label">Password</label>
